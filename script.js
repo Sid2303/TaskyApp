5113599ecbeeebfba6cf30427a0ec7184a258074
Sid2303 <subhashr898@gmail.com>
2024-02-18 18:36:47 +0530

    First update d2

---
+++ script.js
@@ -0,0 +1,57 @@
+var state = {
+    taskList: [
+        {
+            imageUrl: "",
+            taskTitle: "",
+            taskType: "",
+            taskDesc: "",
+        },
+        {
+            imageUrl: "",
+            taskTitle: "",
+            taskType: "",
+            taskDesc: "",
+        },
+        {
+            imageUrl: "",
+            taskTitle: "",
+            taskType: "",
+            taskDesc: "",
+        },
+        {
+            imageUrl: "",
+            taskTitle: "",
+            taskType: "",
+            taskDesc: "",
+        },
+        {
+            imageUrl: "",
+            taskTitle: "",
+            taskType: "",
+            taskDesc: "",
+        },
+        {
+            imageUrl: "",
+            taskTitle: "",
+            taskType: "",
+            taskDesc: "",
+        },
+        {
+            imageUrl: "",
+            taskTitle: "",
+            taskType: "",
+            taskDesc: "",
+        },
+    ]
+}
+
+const state1= {
+    taskList:[],
+};
+
+//DOM
+
+const taskContents= document.querySelector(".task_content");
+const taskModal= document.querySelector(".task_modal_body");
+
+console.log(taskContents);
